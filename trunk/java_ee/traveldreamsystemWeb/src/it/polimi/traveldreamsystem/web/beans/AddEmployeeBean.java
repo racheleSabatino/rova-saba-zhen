1b2b8ffd35de3de01c880350f6b6feab6596c3b0
nearffxx@gmail.com <nearffxx@gmail.com@aa1895d1-f6e1-e567-0b98-ca3dce7d887b>
2014-01-12 18:59:41 +0000

    MILESTONE 1 !!!

--- trunk/java_ee/traveldreamsystemWeb/src/it/polimi/traveldreamsystem/web/beans/AddEmployeeBean.java
+++ trunk/java_ee/traveldreamsystemWeb/src/it/polimi/traveldreamsystem/web/beans/AddEmployeeBean.java
@@ -7,30 +7,29 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-
-@ManagedBean(name="addEmployeeBean")
+@ManagedBean(name = "addEmployeeBean")
 @RequestScoped
 public class AddEmployeeBean {
 
 	@EJB
 	private AddNewEmployee addEmployee;
-		
+
 	private UtenteDTO employee;
-		
+
 	public AddEmployeeBean() {
-			this.employee = new UtenteDTO();
-		}
-		
-		public void add() {
-			addEmployee.addNewEmployee(employee);
-		}
-		
-		public UtenteDTO getCurrent() {
-			return employee;
-		}
-		
-		public void setCurrent(UtenteDTO employee) {
-			this.employee = employee;
-		}
-		
+		this.employee = new UtenteDTO();
+	}
+
+	public void add() {
+		addEmployee.addNewEmployee(employee);
+	}
+
+	public UtenteDTO getEmployee() {
+		return employee;
+	}
+
+	public void getEmployee(UtenteDTO employee) {
+		this.employee = employee;
+	}
+
 }
