e35b42201e71ab7699822b5df5856512ef8ee9cf
gabriele.rovaris <gabriele.rovaris@hotmail.it>
2014-01-25 10:55:32 +0000

    stub di account cliente

--- java_ee/traveldreamsystemWeb/src/it/polimi/traveldreamsystem/web/beans/UserBean.java
+++ java_ee/traveldreamsystemWeb/src/it/polimi/traveldreamsystem/web/beans/UserBean.java
@@ -10,6 +10,52 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean
 @RequestScoped
 public class UserBean {
+	private boolean pswPanelVisible = false;
+	private String psw1;
+	private String psw2;
+	private String newPsw;
+	
+	
+
+	public boolean isPswPanelVisible() {
+		return pswPanelVisible;
+	}
+
+	public void setPswPanelVisible(boolean pswPanelVisible) {
+		this.pswPanelVisible = pswPanelVisible;
+	}
+
+	public String getPsw1() {
+		return psw1;
+	}
+
+	public void setPsw1(String psw1) {
+		this.psw1 = psw1;
+	}
+
+	public String getPsw2() {
+		return psw2;
+	}
+
+	public void setPsw2(String psw2) {
+		this.psw2 = psw2;
+	}
+
+	public String getNewPsw() {
+		return newPsw;
+	}
+
+	public void setNewPsw(String newPsw) {
+		this.newPsw = newPsw;
+	}
+
+	public UtenteMgrBeanLocal getUtenteMgrBean() {
+		return utenteMgrBean;
+	}
+
+	public void setUtenteMgrBean(UtenteMgrBeanLocal utenteMgrBean) {
+		this.utenteMgrBean = utenteMgrBean;
+	}
 
 	@EJB
 	private UtenteMgrBeanLocal utenteMgrBean;
@@ -35,6 +81,15 @@ public class UserBean {
 			return "true";
 		}
 		return "false";
+	}
+	
+	public void showPasswordPanel(){
+		pswPanelVisible = true;
+	}
+	
+	public void changePassword(){
+		
+		pswPanelVisible = false;
 	}
 
 	public String getIsRoleCliente() {
