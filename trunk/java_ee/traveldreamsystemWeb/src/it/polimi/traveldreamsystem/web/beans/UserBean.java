bf61b91142e184fee94896070015e8325a1c6928
gabriele.rovaris@hotmail.it <gabriele.rovaris@hotmail.it@aa1895d1-f6e1-e567-0b98-ca3dce7d887b>
2014-01-26 12:06:43 +0000

    working on changepsw

--- trunk/java_ee/traveldreamsystemWeb/src/it/polimi/traveldreamsystem/web/beans/UserBean.java
+++ trunk/java_ee/traveldreamsystemWeb/src/it/polimi/traveldreamsystem/web/beans/UserBean.java
@@ -4,50 +4,25 @@ import it.polimi.traveldreamsystem.SessionBeans.UtenteMgrBeanLocal;
 import it.polimi.traveldreamsystem.dto.UtenteDTO;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 @RequestScoped
 public class UserBean {
-	private boolean pswPanelVisible = false;
-	private String psw1;
-	private String psw2;
-	private String newPsw;
+	private String newPsw1;
+	private String oldPsw;
+	private String newPsw2;
 	
-	
+	@EJB
+	private UtenteMgrBeanLocal utenteMgrBean;
 
-	public boolean isPswPanelVisible() {
-		return pswPanelVisible;
-	}
 
-	public void setPswPanelVisible(boolean pswPanelVisible) {
-		this.pswPanelVisible = pswPanelVisible;
-	}
 
-	public String getPsw1() {
-		return psw1;
-	}
 
-	public void setPsw1(String psw1) {
-		this.psw1 = psw1;
-	}
 
-	public String getPsw2() {
-		return psw2;
-	}
-
-	public void setPsw2(String psw2) {
-		this.psw2 = psw2;
-	}
-
-	public String getNewPsw() {
-		return newPsw;
-	}
-
-	public void setNewPsw(String newPsw) {
-		this.newPsw = newPsw;
-	}
 
 	public UtenteMgrBeanLocal getUtenteMgrBean() {
 		return utenteMgrBean;
@@ -57,8 +32,6 @@ public class UserBean {
 		this.utenteMgrBean = utenteMgrBean;
 	}
 
-	@EJB
-	private UtenteMgrBeanLocal utenteMgrBean;
 
 	public String getName() {
 		return utenteMgrBean.getUtenteDTO().getNome();
@@ -82,14 +55,37 @@ public class UserBean {
 		}
 		return "false";
 	}
-	
-	public void showPasswordPanel(){
-		pswPanelVisible = true;
-	}
-	
-	public void changePassword(){
 		
-		pswPanelVisible = false;
+	public void changePassword(){
+		if (newPsw1 == newPsw2 && (utenteMgrBean.getUtenteDTO()).getPassword() == oldPsw){
+			
+		}
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("Cambio avvenuto con successo"));  
+	}
+
+	public String getNewPsw1() {
+		return newPsw1;
+	}
+
+	public void setNewPsw1(String newPsw1) {
+		this.newPsw1 = newPsw1;
+	}
+
+	public String getOldPsw() {
+		return oldPsw;
+	}
+
+	public void setOldPsw(String oldPsw) {
+		this.oldPsw = oldPsw;
+	}
+
+	public String getNewPsw2() {
+		return newPsw2;
+	}
+
+	public void setNewPsw2(String newPsw2) {
+		this.newPsw2 = newPsw2;
 	}
 
 	public String getIsRoleCliente() {
